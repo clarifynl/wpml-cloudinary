ddb3d4da3d85933cd05f28bb75e878ba8be8b349
Twansparant <toinekamps@gmail.com>
2021-11-15 12:58:21 +0100

    Add boilerplate plugin

---
+++ includes/class-wpml-cloudinary-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    wpml_cloudinary
+ * @subpackage wpml_cloudinary/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    wpml_cloudinary
+ * @subpackage wpml_cloudinary/includes
+ * @author     Your Name <email@example.com>
+ */
+class wpml_cloudinary_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
